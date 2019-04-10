1e5d0d4699627a041251572ae7ab62e17138559a
Erik Dahl <dahlsailrunner@yahoo.com>
2019-04-10 06:02:29 -0500

    removed app name param, updated method name

--- Tccc2019.ApiBooster/SerilogHelpers.cs
+++ Tccc2019.ApiBooster/SerilogHelpers.cs
@@ -21,12 +21,10 @@ namespace Tccc2019.ApiBooster
         /// Provides standardized, centralized Serilog wire-up for a suite of applications.
         /// </summary>
         /// <param name="loggerConfig">Provide this value from the UseSerilog method param</param>
-        /// <param name="provider">Provide this value from the UseSerilog method param as well</param>
-        /// <param name="applicationName">Represents the name of YOUR APPLICATION and will be used to segregate your app
-        /// from others in the logging sink(s).</param>
+        /// <param name="provider">Provide this value from the UseSerilog method param as well</param>       
         /// <param name="config">IConfiguration settings -- generally read this from appsettings.json</param>
-        public static void WithSimpleConfiguration(this LoggerConfiguration loggerConfig, 
-            IServiceProvider provider, string applicationName, IConfiguration config)
+        public static void WithTcccConfiguration(this LoggerConfiguration loggerConfig, 
+            IServiceProvider provider, IConfiguration config)
         {
             var name = Assembly.GetEntryAssembly().GetName();
 
